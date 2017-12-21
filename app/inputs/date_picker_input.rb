7e168003b8212261ad0bf8f35548040302e197bc
Matías Pierobon <matias.pierobonm@gmail.com>
2017-12-21 14:05:49 -0300

    add a custom datepicker elemento to simple form

---
+++ app/inputs/date_picker_input.rb
@@ -0,0 +1,22 @@
+class DatePickerInput < SimpleForm::Inputs::Base
+  def input
+    template.content_tag(:div, class: 'input-group date form_datetime') do
+      template.concat @builder.text_field(attribute_name, input_html_options)
+      template.concat span_icon
+    end
+  end
+
+  def input_html_options
+    super.merge(class: 'form-control datepicker no-readonly', readonly: true)
+  end
+
+  def span_icon
+    template.content_tag(:span, class: 'input-group-addon') do
+      template.concat icon_date
+    end
+  end
+
+  def icon_date
+    "<i class='material-icons'>event</i>".html_safe
+  end
+end
