cd349a523d7031af9c31c2736b9b176ba8617c42
Matias Pierobon <matias.pierobonm@gmail.com>
2018-02-01 23:58:48 -0300

    add base validators for test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,13 @@
 class Test < ApplicationRecord
   belongs_to :course
+
+  validates :date, precence: true
+  validates :date, uniqueness: { scope: :course }
+  validates :title, precence: true
+  validates :course, precence: true
+
+  validate do
+    errors.add(:date, :invalid) if course.isNull? &&
+                                   course.from <= date <= course.end
+  end
 end
