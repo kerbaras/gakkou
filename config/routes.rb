c4336b796583212ff8f7e4832dd61a3b8320ed32
Matias Pierobon <matias.pierobonm@gmail.com>
2018-02-07 19:52:25 -0300

    route assistances

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :courses do
     resources :enrollments, only: %i[new create destroy]
     resources :tests, except: %i[index show]
+    resources :assistances, only: %i[create destroy update]
   end
 
   root 'courses#index'
