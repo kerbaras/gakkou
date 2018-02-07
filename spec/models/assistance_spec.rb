8ecf0b9ece33151d940060b636187d27e046061e
Matias Pierobon <matias.pierobonm@gmail.com>
2018-02-07 19:52:13 -0300

    add base test for assistances

---
+++ spec/models/assistance_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Assistance, type: :model do
+  it 'should de from a student of the course' do
+  end
+
+  it 'should has a normalize grade' do
+  end
+
+  it 'should understand approved?' do
+  end
+
+  it 'should undertand graded?' do
+  end
+end
